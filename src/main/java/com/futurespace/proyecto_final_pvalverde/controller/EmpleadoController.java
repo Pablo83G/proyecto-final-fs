fcdf2d478b6ce89690364be7a379bfd997242a48
Pablo83G <pablovalverde83@gmail.com>
2024-05-14 18:27:14 +0200

    borrar y crear empleados

--- src/main/java/com/futurespace/proyecto_final_pvalverde/controller/EmpleadoController.java
+++ src/main/java/com/futurespace/proyecto_final_pvalverde/controller/EmpleadoController.java
@@ -3,7 +3,9 @@ package com.futurespace.proyecto_final_pvalverde.controller;
 import com.futurespace.proyecto_final_pvalverde.entities.Empleado;
 import com.futurespace.proyecto_final_pvalverde.services.EmpleadoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,19 +19,45 @@ public class EmpleadoController {
     private EmpleadoService empleadoService;
 
 
-    @PostMapping
+    @PostMapping("/createEmpleado")
     public ResponseEntity<Object> addEmpleado(@RequestBody Empleado empleado){
         return empleadoService.addEmpleado(empleado);
     }
 
 
-    @GetMapping
-    public List<Empleado> getEmpleado(){
+    @GetMapping("/getEmpleado")
+    public ResponseEntity<List<Empleado>> getEmpleado(){
         return empleadoService.getEmpleado();
     }
 
+    // Actualizar la información sobre los usuarios
+   /*
+    @PutMapping (path = "/{userId}",
 
+            consumes = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            },
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            })
 
+    public UserRest updateUser(@PathVariable String userId, @Validated @RequestBody UpdateUserDetailsRequestModel userDetails) {
+        //Utilizamos el objeto de clase UserRest para tener sus atributos y métodos
+        UserRest storeUserDetails = users.get(userId); // objeto datos almacenados
+        storeUserDetails.setFirstname(userDetails.getFirstName()); // establecer nuevo nombre usando el objeto userDetails
+        storeUserDetails.setLastname(userDetails.getLastName()); // establecer nuevo apellido
+        users.put(userId, storeUserDetails); // actualizar usuarios para devolver la información almacenada
+
+        return storeUserDetails;
+    }
+*/
+    //Borrar usuario
+    @DeleteMapping (path = "deleteById/{id_empleado}")
+    public ResponseEntity<Object> deleteIdEmpleado(@PathVariable int id_empleado){
+        return empleadoService.deleteIdEmpleado(id_empleado);
+    }
 
 
 }
--- src/main/java/com/futurespace/proyecto_final_pvalverde/services/EmpleadoService.java
+++ src/main/java/com/futurespace/proyecto_final_pvalverde/services/EmpleadoService.java
@@ -2,15 +2,14 @@ package com.futurespace.proyecto_final_pvalverde.services;
 
 import com.futurespace.proyecto_final_pvalverde.entities.Empleado;
 import com.futurespace.proyecto_final_pvalverde.repositories.IEmpleadosRepositorio;
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpleadoService {
@@ -27,22 +26,36 @@ public class EmpleadoService {
 
     // Insertar datos
     public ResponseEntity<Object> addEmpleado(Empleado empleado){
-        ResponseEntity<Object> r = null;
+        ResponseEntity<Object> resp = null;
         try{
-            Empleado e = iEmpleadosRepositorio.save(empleado);
-            r = new ResponseEntity<>(e, HttpStatus.OK);
-            return r;
-        }catch(DataIntegrityViolationException e){
-            r = new ResponseEntity<>("Ha habido un error", HttpStatus.BAD_REQUEST);
+            Empleado empl = iEmpleadosRepositorio.save(empleado);
+            resp = new ResponseEntity<>(empl, HttpStatus.OK);
+            return resp;
+        }catch(DataIntegrityViolationException excep){
+            resp = new ResponseEntity<>("Ha habido un error", HttpStatus.BAD_REQUEST);
 
-            System.out.print("Error: " + e.getMessage());
-            return r;
+            System.out.print("Error: " + excep.getMessage());
+            return resp;
 
         }
     }
 
     //Conseguir datos
-    public List<Empleado> getEmpleado(){ return iEmpleadosRepositorio.findAll();
+    public ResponseEntity<List<Empleado>> getEmpleado(){
+        List<Empleado> emplList = iEmpleadosRepositorio.findAll();
+        return new ResponseEntity<>(emplList, HttpStatus.OK);
+
     }
+
+    //Borrar datos
+    public ResponseEntity<Object> deleteIdEmpleado(int id_empleado){
+        ResponseEntity<Object> resp = null;
+        //Optional<Empleado> empleadoId = iEmpleadosRepositorio.findById(id_empleado);
+        iEmpleadosRepositorio.deleteById(id_empleado);
+        resp = new ResponseEntity<>("Empleado eliminado correctamente", HttpStatus.OK);
+        return resp;
+    }
+//Borrar por Nif
+
 
 }
